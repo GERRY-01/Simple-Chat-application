d77f8aff337bffe470c651628d2b1062df935312
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-11 13:48:08 +0300

    added a path for logout

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('registration', views.registration, name='registration'),
     path('login', views.login, name='login'),
+    path('logout', views.logout_user, name='logout'),
     path('home', views.home, name='home')
 ]
